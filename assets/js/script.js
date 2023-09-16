025d930ba67b623ccd491655c343838d38449733
Logan Lagrange <lagrangelogan@gmail.com>
2023-09-15 22:10:34 -0400

    Added html elements, basic css styling, and questions to js

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+let questions = [
+    {
+        question: "What is the abbreviation for tablespoon?",
+        choices: ["tsp", "Tbsp", "t", "C"],
+        answer: "Tbsp"
+    }, {
+        question: "What is the name of the technique use to incorporate air into a product?",
+        choices: ["Beat", "Fold", "Chiffonade", "Whip"],
+        answer: "Whip"
+    }, {
+        question: "What leavening agent is also commonly used as a household cleaner?",
+        choices: ["Baking Powder", "Yeast", "Baking Soda", "Vinegar"],
+        answer: "Baking Soda"
+    }, {
+        question: "What is the name of the classical technique to decoratively peel a mushroom or other item?",
+        choices: ["Peel", "Pare", "Flute", "Skin"],
+        answer: "Flute"
+    }
+]
+
