0c3d155446b7f21c3a1cf9fec47c0ffa579df403
Logan Lagrange <lagrangelogan@gmail.com>
2023-09-16 22:32:43 -0400

    created timer and question display and functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 let questions = [
     {
-        question: "What is the abbreviation for tablespoon?",
-        choices: ["tsp", "Tbsp", "t", "C"],
-        answer: "Tbsp"
+        question : "What is the abbreviation for tablespoon?",
+        choices : ["tsp", "Tbsp", "t", "C"],
+        answer : "Tbsp"
     }, {
         question: "What is the name of the technique use to incorporate air into a product?",
         choices: ["Beat", "Fold", "Chiffonade", "Whip"],
@@ -18,3 +18,106 @@ let questions = [
     }
 ]
 
+
+
+// Hide quiz and leaderboard pages by default
+var quizPage =  document.getElementById("quiz-pg");
+quizPage.style.display =  "none";
+
+var leaderboardPage = document.getElementById("leaderboard-pg");
+leaderboardPage.style.display =  "none";
+
+// Grab timer heading
+var timerEl = document.getElementById("countdown");
+// Function for timer
+function countdown() {
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + " seconds remaining";
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent =  timeLeft + " second remaining";
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Time's Up!";
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
+// Grabs question header
+var questionHeader = document.getElementById("question-header");
+// Grabs option buttons and stores in an array
+var options = [];
+options = document.querySelectorAll(".option");
+// Keeps track of which question is currently active
+var currentQues = 0;
+// Creates default value for time to do quiz
+var timeLeft = 120;
+
+// Function that executes when start button is clicked, starts timer and displays first question
+function startQuiz() {
+    // Timer
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + " seconds remaining";
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent =  timeLeft + " second remaining";
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Time's Up!";
+            clearInterval(timeInterval);
+            finish();
+        }
+    }, 1000);
+    
+    // Calls function that controls which question is displayed
+    display(currentQues);
+    for (var i = 0; i < options.length; i++){
+        options[i].addEventListener("click", checkAns);
+    }
+}
+    
+    
+function display(index) {
+    var ques = questions[index];
+    questionHeader.textContent = ques.question;
+
+    for (var i = 0; i < options.length; i++) {
+        options[i].textContent = ques.choices[i];
+    }
+}
+
+function checkAns() {
+    var userSelect = this.textContent;
+    ques = questions[currentQues];
+
+    if (userSelect === ques.answer) {
+        currentQues++;
+        if (currentQues < questions.length)  {
+            display(currentQues);
+        } else {
+            finish();
+        }
+    } else {
+        timeLeft -= 5;
+    }
+}
+
+function finish() {
+    
+}
+    
+
+
+// Grab elements for start button event listener
+var startButton = document.querySelector(".start-btn");
+var startPage = document.getElementById("start-pg");
+// Event  listener for start button
+startButton.addEventListener("click", function() {
+    // countdown();
+    startPage.style.display = "none";
+    quizPage.style.display = "flex";
+    startQuiz()
+})
